cb8f9742654e163f87df4e32c4bef01ce5e0631f
JoeJoeNguyen <snguyen7@stevens.edu>
2024-01-28 19:18:51 -0500

    correct up to test 11

--- hw1/test/junk.sh
+++ hw1/test/junk.sh
@@ -7,17 +7,46 @@ usage() {
 cat << EOF
 Usage: $0 [-hlp] [list of files]
  -h: Display help.
- -l: List junked files
- -p: Purge all files
+ -l: List junked files.
+ -p: Purge all files.
 [list of files] with no other arguments to junk those files.
 EOF
-  exit 1
 }
 exit_abnormal() {
   usage
-  exit
+  exit 1
 }
-flag_counter=0
+# Initialize a counter for the number of options enabled
+option_counter=0
+
+# Check each option
+while getopts ":hlp" opt; do
+  case ${opt} in
+    h )
+      option_counter=$((option_counter+1))
+      ;;
+    l )
+      option_counter=$((option_counter+1))
+      ;;
+    p )
+      option_counter=$((option_counter+1))
+      ;;
+    *)
+      echo "Error: Unknown option '-${OPTARG}'." >&2
+      exit_abnormal
+      ;;
+  esac
+done
+
+shift $((OPTIND -1))
+
+# If more than one option is enabled, print an error message and exit
+if [ $option_counter -gt 1 ] || ([ $option_counter -gt 0 ] && (( $# > 0 ))); then
+  echo "Error: Too many options enabled."
+  usage
+  exit 1
+fi
+
 
 # Check if the ~/.junk directory exists and create it if it does not
 mkdir -p ~/.junk
@@ -25,15 +54,12 @@ mkdir -p ~/.junk
 while getopts ":hlp" options; do
   case "${options}" in
     h)
-      ((flag_counter++))
       usage
       ;;
     l)
-      ((flag_counter++))
       ls -a -l ~/.junk/
       ;;
     p)
-      ((flag_counter++))
       #delete all file "*" and hidden files ".*"
       rm -rf ~/.junk/{*,.*}
       ;;
@@ -42,19 +68,13 @@ while getopts ":hlp" options; do
       exit_abnormal
       ;;
   esac
-  if ((flag_counter > 1)); then
-    echo "Error: Too many options enabled."
-    exit_abnormal
-  fi
 done
 
-shift $((OPTIND -1))
-
-if ((flag_counter > 0)) && (( $# > 0 )); then
-  echo "Error: Too many options supplied."
-  exit_abnormal
+# Check if no arguments were provided
+if [ $# -eq 0 ]; then
+  usage
+  exit 0
 fi
-
 
 
 # Move the files to the ~/.junk directory if file not found get warning
