a8ed06ac94cd2190da76db8f9242bb0afb6b9d41
JoeJoeNguyen <snguyen7@stevens.edu>
2024-03-21 04:24:16 -0400

    hw4

---
+++ hw4/maxfile.c
@@ -0,0 +1,13 @@
+//
+// Created by Joe Nguyen on 3/20/24.
+//
+
+
+int main(int argc, char** argv ){
+    if(argc != 2){
+        fprint(sterr, "Usage: %s <directory>\n", argv[0]);
+    }
+    //create a directory of any name using mkdir
+
+    //within that directory make at least 2 subdirectories 
+}
