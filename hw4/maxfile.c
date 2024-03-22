621a2aabc4c251738a12fe16cbb40678d1ba178c
JoeJoeNguyen <snguyen7@stevens.edu>
2024-03-21 21:48:43 -0400

    hw4 continue

--- hw4/maxfile.c
+++ hw4/maxfile.c
@@ -9,17 +9,32 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
+
+void super(char *fpath, char* LargestFile, int *size){
+
+}
+
+
+
 int main(int argc, char** argv ){
     if(argc != 2){
         fprintf(stderr, "Usage: %s <directory>\n", argv[0]);
     }
-    //create a directory of any name using mkdir
-    mkdir(argv[1], S_IRUSR | S_IWUSR | S_IXUSR);
+    DIR* direc = opendir(argv[1]);
 
-    //within that directory make at least 2 subdirectories
-    char sub1[256], sub2[256];
-    sprintf(sub1, "%s/sub1", argv[1]);
-    sprintf(sub2, "%s/sub2", argv[1]);
-    mkdir(sub1, S_IRUSR | S_IWUSR | S_IXUSR);
-    mkdir(sub2, S_IRUSR | S_IWUSR | S_IXUSR);
+    if(direc == NULL){
+        printf(stderr, "Cannot open the directory %s", argv[1]);
+    }
+
+    struct dirent *entry;
+    char *path[PATH_MAX];
+    while((entry = readdir(direc)) != NULL){
+        //entry->d_name;
+        snprintf(path, PATH_MAX, "%s/%s", argv[0]);
+
+    }
+
+
+
 }
